d387690c18f68d1eeb94e4face3434eeffdb625c
canaydogan <canaydogan0568@gmail.com>
2025-01-16 14:06:28 +0300

    Some map examples

--- src/OOP_learn/Main.java
+++ src/OOP_learn/Main.java
@@ -1,7 +1,34 @@
 package OOP_learn;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        int counter, wordCounter, inputCounter = 0;
 
+        // Safety Preventions about user input
+        try {
+            System.out.println("How many words are you going to type?");
+            input = scanner.nextLine();
+            inputCounter = Integer.parseInt(input);
+            if (inputCounter == 0) return;
+            else if (inputCounter < 0) {
+                System.out.println("Invalid value error. Program has been terminated");
+                return;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Invalid value error. Program has been terminated.");
+        }
+
+        Map map = new HashMap();
+        for (counter = 0; counter < inputCounter; counter++) {
+            System.out.print("Type the word #" + (counter + 1) + ": ");
+            input = scanner.nextLine();
+            map.put(input, input.length());
+        }
+
+        System.out.println(map);
     }
 }
